6a92b87a8bafab3ef30bcf5ec62f16caa956d1f7
ilhemboud05 <ilhemmiliboud@gmail.com>
2026-03-04 18:02:42 +0100

    modification age

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Bonjour Ilhem !");
         System.out.println("Bonjour !");
          System.out.println("hello my name is ilhem");
+          System.out.println("age 21");
     }
 }
